76a4ebfc9dff515363723d759c9a405ceb119539
Elizabeth Korzhova <elizabeth.korzhova.coop@gmail.com>
2026-04-21 21:03:29 +0300

    fix: add BACKEND_HOST to settings.py

--- learn_studio/settings.py
+++ learn_studio/settings.py
@@ -14,7 +14,6 @@ import datetime
 from pathlib import Path
 from dotenv import load_dotenv
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,7 +29,7 @@ REFRESH_TOKEN_LIFETIME = datetime.timedelta(days=1)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("BACKEND_HOST")]
 
 # Application definition
 
@@ -197,7 +196,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
 
 
 MEDIA_URL = "/media/"
